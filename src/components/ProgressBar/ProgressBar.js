21818fe2606d406b364ac07f03396ab7d8999bac
patlo-git <56564279+patlo-git@users.noreply.github.com>
2022-08-17 16:48:30 -0700

    Implement large progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,31 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return (
+  <ProgressStyling max="100" value={value}></ProgressStyling>
+  )
 };
+
+const ProgressStyling = styled.progress`
+  -webkit-appearance: none;
+  appearance: none;
+  width: 370px;
+  height: 24px;
+
+  &::-webkit-progress-bar {
+    padding: 4px;
+    background-color: #eee;
+    border-radius: 4px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+  }
+ 
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: 4px; 
+    background-size: 35px 20px, 100% 100%, 100% 100%;
+  }
+`;
 
 export default ProgressBar;
