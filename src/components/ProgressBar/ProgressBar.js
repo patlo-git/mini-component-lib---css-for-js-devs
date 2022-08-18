aad941756576a3e8a059e1499466aaf9283ab927
patlo-git <56564279+patlo-git@users.noreply.github.com>
2022-08-18 11:57:25 -0700

    Satisfy all requirements for Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -9,18 +9,18 @@ import VisuallyHidden from '../VisuallyHidden';
 const SIZES = {
   small: {
     "--height": 8 + "px",
-    "--borderRadius": "4px 0 0 4px",
     "--padding": 0,
+    "--borderRadius": 4 + "px",
   },
   medium: {
     "--height": 12 + "px",
-    "--borderRadius": "4px 0 0 4px",
-    "padding": 0,
+    "--padding": 0,
+    "--borderRadius": 4 + "px",
   },
   large: {
     "--height": 24 + "px",
-    "--borderRadius": "4px 0 0 4px",
     "--padding": 4 + "px",
+    "--borderRadius": 8 + "px",
   }
 }
 
@@ -48,14 +48,13 @@ const ProgressBarBase = styled.progress`
   &::-webkit-progress-bar {
     padding: var(--padding);
     background-color: ${COLORS.transparentGray15};
-    // how to apply an outer progress bar border-radius of 8px only when the bar is Large style?
-    border-radius: 4px; 
+    border-radius: var(--borderRadius); 
     box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   }
  
   &::-webkit-progress-value {
     background-color: ${COLORS.primary};
-    border-radius: var(--borderRadius); 
+    border-radius: 4px 0 0 4px; 
   }
 `;
 
