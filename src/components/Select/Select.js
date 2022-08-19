67084155fe068fc31e0d459457223ce1ff4d851e
patlo-git <56564279+patlo-git@users.noreply.github.com>
2022-08-19 12:03:11 -0700

    Implement some initial styling of the Select element

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -2,17 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectForm value={value} onChange={onChange}>
+        {children}
+      </SelectForm>
+    </Wrapper>
   );
 };
+
+const SelectForm = styled.select`
+  display: block;
+  // -webkit-appearance: none; 
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  
+  // &::after {
+  //   content: "▼";
+  //   font-size: 1rem;
+  //   top: 6px;
+  //   right: 10px;
+  //   position: absolute;
+  // }
+`;
+
+
+// TODO: Style the chevron
 
 export default Select;
