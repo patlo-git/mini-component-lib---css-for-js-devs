6ccc721b18dfc7bf8ed1c81dc9511d2980dddf80
patlo-git <56564279+patlo-git@users.noreply.github.com>
2022-08-19 09:58:28 -0700

    Implement post-comparison changes to progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -9,16 +9,16 @@ import VisuallyHidden from '../VisuallyHidden';
 const STYLES = {
   small: {
     height: 8,
-    padding: 0,
+    // padding: 0,
     radius: 4,
   },
   medium: {
     height: 12,
-    padding: 0,
+    // padding: 0,
     radius: 4,
   },
   large: {
-    height: 24,
+    height: 16,
     padding: 4,
     radius: 8,
   }
@@ -27,9 +27,9 @@ const STYLES = {
 const ProgressBar = ({ value, size }) => {
   const styles = STYLES[size];
 
-  // if (!styles) {
-  //   throw new Error(`No styles found`);
-  // }
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
 
   return (
     <Wrapper
@@ -44,7 +44,7 @@ const ProgressBar = ({ value, size }) => {
         '--padding': styles.padding + 'px',
       }}
       >
-      <VisuallyHidden>{value}</VisuallyHidden>
+      <VisuallyHidden>{value}%</VisuallyHidden>
       <BarWrapper>
         <ProgressBarValue
           style={{
@@ -63,13 +63,13 @@ const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: var(--radius);
-  overflow: hidden;
-  }
 `;
 
 const BarWrapper = styled.div`
-  overflow: hidden;
   border-radius: 4px;
+
+  // Trims corners when progress bar value approaches 100%
+  overflow: hidden;
 `;
 
 const ProgressBarValue = styled.div`
