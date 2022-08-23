74fdd78b13b5ff9887cd8511758e44c0ba0b78b3
patlo-git <56564279+patlo-git@users.noreply.github.com>
2022-08-23 12:54:43 -0700

    Implement post-comparison improvements

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,17 +9,15 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const STYLES = {
   small: {
-    fontSize: 14/16,
-    borderBottom: 1,
-    iconSize: 16/16,
-    height: 24/16,
+    fontSize: 14,
+    iconSize: 16,
+    height: 24,
     stroke: 1,
   },
   large: {
-    fontSize: 18/16,
-    borderBottom: 2,
-    iconSize: 24/16,
-    height: 36/16,
+    fontSize: 18,
+    iconSize: 24,
+    height: 36,
     stroke: 2,
   },
 }
@@ -29,7 +27,7 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
   
   const styles = STYLES[size];
@@ -37,30 +35,26 @@ const IconInput = ({
   return (
     <Wrapper>
       <VisuallyHidden>{label}</VisuallyHidden>
-      <TextInput
-        size={size}
-        placeholder={placeholder}
-        aria-label="Search Input Form"
-        style={{
-          '--height': styles.height + 'rem',
-          '--left': styles.height + 'rem',
-          '--fontSize': styles.fontSize + 'rem',
-          '--borderBottom': styles.borderBottom + 'px',
-          '--padding': (styles.iconSize / 4) + 'rem',
-          '--width': width + 'px',
-        }}
-        ></TextInput>
       <IconWrapper
-        size={size}
         style={{
-          '--iconSize': styles.iconSize + 'rem',
+          '--iconSize': styles.iconSize + 'px',
           }}>
         <Icon
           id={icon}
-          size={size} 
+          size={styles.iconSize} 
           strokeWidth={styles.stroke}
           ></Icon>
       </IconWrapper>
+      <TextInput
+        aria-label="Search Input Form"
+        {...delegated}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height + 'px',
+          '--fontSize': styles.fontSize / 16 + 'rem',
+          '--stroke': styles.stroke + 'px',
+        }}
+        ></TextInput>
     </Wrapper>
   );
 };
@@ -68,28 +62,8 @@ const IconInput = ({
 const Wrapper = styled.label`
   display: block;
   position: relative;
-`;
-
-const TextInput = styled.input`
-  appearance: none;
-  border: none;
-  border-bottom: var(--borderBottom) solid ${COLORS.black};
   color: ${COLORS.gray700};
-  font-weight: 700;
-  font-size: var(--fontSize);
-  padding: var(--padding) 0;
-  padding-left: var(--left);
-  width: var(--width);
 
-  &::placeholder {
-    font-weight: 400;
-    color: ${COLORS.gray500};
-  }
-  
-  &:focus {
-    outline-offset: 2px;
-  }
-  
   &:hover {
     color: ${COLORS.black};
   }
@@ -100,13 +74,25 @@ const IconWrapper = styled.div`
   top: 0;
   left: 0;
   bottom: 0;
-  width: var(--iconSize);
   height: var(--iconSize);
   margin: auto 0;
-  color: ${COLORS.gray700};
+`;
 
-  ${TextInput}:hover + & {
-    color: ${COLORS.black};
+const TextInput = styled.input`
+  appearance: none;
+  border: none;
+  border-bottom: var(--stroke) solid ${COLORS.black};
+  color: inherit;
+  font-weight: 700;
+  font-size: var(--fontSize);
+  padding-left: var(--height);
+  width: var(--width);
+  height: var(--height);
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
   }
 `;
 
